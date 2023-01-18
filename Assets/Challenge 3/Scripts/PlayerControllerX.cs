81f4ce45261dc15aae868cd8b6665fe8765dea11
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2023-01-17 20:39:53 -0500

    finish lower-boundary, qol, sound + effects

--- Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -11,12 +11,14 @@ public class PlayerControllerX : MonoBehaviour
     public Rigidbody playerRb;
 
     public float upperLimit = 14;
-    public float lowerLimit = 2;
+    //public float lowerLimit = 2;
 
     public ParticleSystem explosionParticle;
     public ParticleSystem fireworksParticle;
 
     private AudioSource playerAudio;
+
+    public AudioClip music;
     public AudioClip moneySound;
     public AudioClip explodeSound;
     public AudioClip bounceSound;
@@ -29,18 +31,29 @@ public class PlayerControllerX : MonoBehaviour
         playerAudio = GetComponent<AudioSource>();
 
         // Apply a small upward force at the start of the game
-        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+        playerRb.AddForce(Vector3.up * 4, ForceMode.Impulse);
+
+        playerAudio.PlayOneShot(music, .01f);
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
-        {
+        if (Input.GetKey(KeyCode.Space) && !gameOver) {
             playerRb.AddForce(Vector3.up * floatForce);
         }
+        /*
+        if (transform.position.y < lowerLimit && !gameOver) {
+            transform.position = new Vector3(transform.position.x, lowerLimit, transform.position.z);
+            playerAudio.PlayOneShot(bounceSound, 1f);
+        }
+        */
+        if (transform.position.y > upperLimit) {
+            transform.position = new Vector3(transform.position.x, upperLimit, transform.position.z);
+            playerRb.velocity = Vector3.zero;
+        }
+
     }
 
     private void OnCollisionEnter(Collision other)
@@ -49,25 +62,24 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Bomb"))
         {
             explosionParticle.Play();
-            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            playerAudio.PlayOneShot(explodeSound, .2f);
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
+            gameObject.GetComponent<BoxCollider>().enabled = false;
         } 
 
         // if player collides with money, fireworks
         else if (other.gameObject.CompareTag("Money"))
         {
             fireworksParticle.Play();
-            playerAudio.PlayOneShot(moneySound, 1.0f);
+            playerAudio.PlayOneShot(moneySound, .2f);
             Destroy(other.gameObject);
 
         }
 
-        if (other.gameObject.tag == "Ground") {
-            playerAudio.PlayOneShot(bounceSound, 1.0f);
-        }
-
+        else if (other.gameObject.tag == "Ground")
+            playerAudio.PlayOneShot(bounceSound, .6f);
     }
 
 }
